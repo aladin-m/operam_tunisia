7a9c1e3c9b05031f477208de718b04062018b5e5
aladin-m <aladinemandhouj@gmail.com>
2023-12-14 15:09:38 +0100

    init

--- operam_tunisia_front/src/main.jsx
+++ operam_tunisia_front/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Layout from './Layout.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Layout></Layout>
   </React.StrictMode>,
 )
