1ffc10777959b902f6462a79fa4d25b9ab31fcd8
aladin-m <aladinemandhouj@gmail.com>
2023-12-14 14:26:52 +0100

    init

--- operam_tunisia_front/src/App.jsx
+++ operam_tunisia_front/src/App.jsx
@@ -2,23 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Layout from './Layout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Layout />
-      
-    </>
-  )
-}
-
-export default App
-
-
-{/* <div>
+      <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -37,4 +27,9 @@ export default App
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p> */}
+      </p>
+    </>
+  )
+}
+
+export default App
